d1981ba04702c96120e10b3e96d18fff3e860c50
Mark Hayden <mark.hayden@gmail.com>
2022-10-29 13:12:26 -0400

    Project setup

---
+++ 2021/01/solution.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/rustylampshade/advent-of-go/shared"
+)
+
+func main() {
+	input := shared.ReadIntFromLine("./input.txt")
+
+	fmt.Print(input)
+}
---
+++ shared/input.go
@@ -0,0 +1,28 @@
+package shared
+
+import (
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func ReadIntFromLine(filename string) (result []int) {
+	content, err := ioutil.ReadFile(filename)
+	ActOn(err)
+
+	for _, line := range strings.Split(string(content), "\n") {
+		number, err := strconv.Atoi(line)
+
+		if err != nil {
+			continue
+		}
+
+		result = append(result, number)
+	}
+
+	if len(result) == 0 {
+		return nil
+	}
+
+	return
+}
